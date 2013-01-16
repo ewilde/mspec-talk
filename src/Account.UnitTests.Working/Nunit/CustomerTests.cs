ebc62aa1927be7757ef1dd9c0b890033116e7fc5
ewilde <ewilde@gmail.com>
2013-01-16 22:26:33 +0000

    update working and set blank project up correctly

--- src/Account.UnitTests.Working/Nunit/CustomerTests.cs
+++ src/Account.UnitTests.Working/Nunit/CustomerTests.cs
@@ -13,11 +13,10 @@ namespace Accounts.UnitTests.Nunit
     public class CustomerTests
     {
         [Test]
-        public void Transfering_money_between_accounts_when_in_credit()
+        public void transfering_money_between_account_with_sufficient_funds_should_credit_target_account()
         {
-            Customer customer = CustomerFactory.CustomerWithSavingAndCurrentAccount();
-            customer.SavingsAccount.Credit(150m);
-        
+            Customer customer = CustomerFactory.CustomerWithSavingAndCurrentAccount(currentAccountCredit: 0m, savingsAccountCredit: 100m);
+
             customer.SavingsAccount.Transfer(customer.CurrentAccount, 100m);
 
             Assert.That(customer.CurrentAccount.Balance, Is.EqualTo(100m));
